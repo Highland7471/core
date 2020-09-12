46b1f2965ea3430b68f4e928540db94027976977
Chris Talkington <chris@talkingtontech.com>
2020-09-11 20:57:49 -0500

    Update test_init.py

--- tests/components/canary/test_init.py
+++ tests/components/canary/test_init.py
@@ -1,11 +1,8 @@
 """The tests for the Canary component."""
-from pytest import fixture
-
 from homeassistant.setup import setup_component
 import homeassistant.components.canary as canary
 
 from tests.async_mock import patch
-from tests.common import get_test_home_assistant
 
 
 def test_setup_with_valid_config(hass, canary) -> None:
@@ -23,18 +20,3 @@ def test_setup_with_valid_config(hass, canary) -> None:
         return_value=True,
     ):
         assert setup_component(hass, canary.DOMAIN, config)
-
-    mock_update.assert_called_once_with()
-    mock_login.assert_called_once_with()
-
-
-def test_setup_with_missing_username(hass, canary) -> None:
-    """Test setup with missing username."""
-    config = {"canary": {"password": "bar"}}
-    assert not setup_component(hass, canary.DOMAIN, config)
-
-
-def test_setup_with_missing_password(hass, canary):
-    """Test setup with missing password."""
-    config = {"canary": {"username": "foo@bar.org"}}
-    assert not setup_component(self.hass, canary.DOMAIN, config)
