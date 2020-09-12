02d2ea4536ee618ca2248ee3d3fe696313e47782
Chris Talkington <chris@talkingtontech.com>
2020-09-11 20:26:08 -0500

    Update test_init.py

--- tests/components/canary/test_init.py
+++ tests/components/canary/test_init.py
@@ -1,13 +1,14 @@
 """The tests for the Canary component."""
-import unittest
+from pytest import fixture
 
-from homeassistant import setup
+from homeassistant.setup import setup_component
 import homeassistant.components.canary as canary
 
 from tests.async_mock import MagicMock, PropertyMock, patch
 from tests.common import get_test_home_assistant
 
 
+@fixture
 def mock_device(device_id, name, is_online=True, device_type_name=None):
     """Mock Canary Device class."""
     device = MagicMock()
@@ -20,6 +21,7 @@ def mock_device(device_id, name, is_online=True, device_type_name=None):
     return device
 
 
+@fixture
 def mock_location(name, is_celsius=True, devices=None):
     """Mock Canary Location class."""
     location = MagicMock()
@@ -29,6 +31,7 @@ def mock_location(name, is_celsius=True, devices=None):
     return location
 
 
+@fixture
 def mock_reading(sensor_type, sensor_value):
     """Mock Canary Reading class."""
     reading = MagicMock()
@@ -37,47 +40,34 @@ def mock_reading(sensor_type, sensor_value):
     return reading
 
 
-class TestCanary(unittest.TestCase):
-    """Tests the Canary component."""
+@patch("homeassistant.components.canary.CanaryData.update")
+@patch("canary.api.Api.login")
+def test_setup_with_valid_config(hass, mock_login, mock_update) -> None:
+    """Test setup with valid YAML."""
+    config = {"canary": {"username": "foo@bar.org", "password": "bar"}}
 
-    def setUp(self):
-        """Initialize values for this test case class."""
-        self.hass = get_test_home_assistant()
-        self.addCleanup(self.tear_down_cleanup)
+    with patch(
+        "homeassistant.components.canary.alarm_control_panel.setup_platform",
+        return_value=True,
+    ), patch(
+        "homeassistant.components.canary.camera.setup_platform",
+        return_value=True,
+    ), patch(
+        "homeassistant.components.canary.sensor.setup_platform",
+        return_value=True,
+    ):
+        assert setup_component(self.hass, canary.DOMAIN, config)
 
-    def tear_down_cleanup(self):
-        """Stop everything that was started."""
-        self.hass.stop()
+    mock_update.assert_called_once_with()
+    mock_login.assert_called_once_with()
 
-    @patch("homeassistant.components.canary.CanaryData.update")
-    @patch("canary.api.Api.login")
-    def test_setup_with_valid_config(self, mock_login, mock_update):
-        """Test setup component."""
-        config = {"canary": {"username": "foo@bar.org", "password": "bar"}}
 
-        with patch(
-            "homeassistant.components.canary.alarm_control_panel.setup_platform",
-            return_value=True,
-        ), patch(
-            "homeassistant.components.canary.camera.setup_platform",
-            return_value=True,
-        ), patch(
-            "homeassistant.components.canary.sensor.setup_platform",
-            return_value=True,
-        ):
-            assert setup.setup_component(self.hass, canary.DOMAIN, config)
+def test_setup_with_missing_username(hass) -> None:
+    """Test setup with missing username."""
+    config = {"canary": {"password": "bar"}}
+    assert not setup_component(self.hass, canary.DOMAIN, config)
 
-        mock_update.assert_called_once_with()
-        mock_login.assert_called_once_with()
-
-    def test_setup_with_missing_password(self):
-        """Test setup component."""
-        config = {"canary": {"username": "foo@bar.org"}}
-
-        assert not setup.setup_component(self.hass, canary.DOMAIN, config)
-
-    def test_setup_with_missing_username(self):
-        """Test setup component."""
-        config = {"canary": {"password": "bar"}}
-
-        assert not setup.setup_component(self.hass, canary.DOMAIN, config)
+def test_setup_with_missing_password(hass):
+    """Test setup with missing password."""
+    config = {"canary": {"username": "foo@bar.org"}}
+    assert not setup_component(self.hass, canary.DOMAIN, config)
